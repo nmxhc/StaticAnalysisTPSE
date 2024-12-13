aa840035bc91a0107154c854361364093ee2f77b
betterNameStillPending <user2537@pm.me>
2024-12-13 14:38:26 +0100

    Added Edge, realised Graph Structure changes

--- src/main/java/DOT/API/Graph.java
+++ src/main/java/DOT/API/Graph.java
@@ -8,17 +8,22 @@ import java.util.Set;
  */
 public class Graph<T> {
     private Set<Node<T>> nodes = new HashSet<Node<T>>();
+    private Set<Edge<T>> edges = new HashSet<Edge<T>>();
 
     /**
      * Creates new empty Graph
      */
     public Graph(){}
 
-    public void addNode(Node node){
+    public void addNode(Node<T> node){
         nodes.add(node);
     }
 
     public Set<Node<T>> getNodes() {
         return nodes;
+    }
+
+    public Set<Edge<T>> getEdges(){
+        return edges;
     }
 }
--- src/main/java/DOT/API/Node.java
+++ src/main/java/DOT/API/Node.java
@@ -4,9 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Node<T> {
-    private Set<Node<T>> successors = new HashSet<Node<T>>();
-    private Set<Node<T>> predecessors = new HashSet<Node<T>>();
-    private T value; //Node's name, indicating the method described
+    private T value;
 
     /**
      * Create root Node without parent.
@@ -15,13 +13,6 @@ public class Node<T> {
 
     };
 
-    /**
-     * Create node with
-     * @param newPredecessor node
-     */
-    public void addPredecessor(Node<T> newPredecessor){
-        this.predecessors.add(newPredecessor);
-    }
 
     public void setValue(T val) {
         value = val;
@@ -31,30 +22,12 @@ public class Node<T> {
         return value;
     }
 
-    /**
-     * @return Node's parent
-     */
-    public Set<Node<T>> getPredecessors(){
-        return predecessors;
-    }
 
     /**
-     * Add edge to Node.
-     * @param newSuccessor to be added
+     *
+     * @return
      */
-    public void addSuccessor(Node<T> newSuccessor){
-        successors.add(newSuccessor);
-    }
-
     public String toString() {
         return value.toString();
-    }
-
-    /**
-     * Get all Edges going away from Node as
-     * @return Node[] array
-     */
-    public Set<Node<T>> getSuccessors(){
-        return successors;
     }
 }
