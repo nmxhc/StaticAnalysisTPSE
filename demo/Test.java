eb3edfaf1ec80b29a13ec4789ef40443f76783a3
totallynotadolphin <totallynotadolphin@riseup.net>
2024-12-18 11:37:17 +0000

    CHA prototype (#21)

--- demo/Test.java
+++ demo/Test.java
@@ -1,9 +1,4 @@
-import java.util.Set;
-
 public abstract class Test {
-
-    int foo;
-    Set<Integer> bar;
 
     public static int inc(int x) {
         return add(x, 1);
@@ -15,18 +10,6 @@ public abstract class Test {
 
     public static void print42() {
         System.out.println("42");
-    }
-
-    public static void complicatedMethod() {
-        System.out.println("Hi!");
-
-        int x = 42;
-        x = inc(x);
-        x = add(x, 23);
-
-        System.out.println(x);
-
-        System.out.println(add(1,2));
     }
 
 }
