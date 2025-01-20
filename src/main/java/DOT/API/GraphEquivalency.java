de9633601aad4ce9ec7ae786dc7779402dba00c3
betterNameStillPending <user2537@pm.me>
2025-01-20 11:27:25 +0100

    Realised requested changes to GraphEquivalency. GraphItem is now an object containing 4 lists.

--- src/main/java/DOT/API/GraphEquivalency.java
+++ src/main/java/DOT/API/GraphEquivalency.java
@@ -5,49 +5,46 @@ import java.util.List;
 
 public class GraphEquivalency {
 
-    public static <T> List<List<GraphItem<T>>> missingAndRedundantNodes(Graph<T> refGraph, Graph<T> testGraph) {
+    public static <T> GraphItem<T> missingAndRedundantNodes(Graph<T> refGraph, Graph<T> testGraph) {
 
         /* A List of 4 elements: missing nodes, missing edges, redundant nodes, redundant edges */
-        List<List<GraphItem<T>>> missingOrTooMany = new LinkedList<>();
-        for (int i = 0; i < 4; i++) {
-            missingOrTooMany.add(new LinkedList<>());
-        }
+
+        GraphItem<T> graphItem = new GraphItem<>();
 
         for (Node<T> n : refGraph.getNodes()) {
             if (!testGraph.getNodes().contains(n)) {
-                missingOrTooMany.get(0).add(n);
+                graphItem.missingNodes.add(n);
             }
         }
 
         for (Edge<T> e : refGraph.getEdges()) {
             if (!testGraph.getEdges().contains(e)) {
-                missingOrTooMany.get(1).add(e);
+                graphItem.missingEdges.add(e);
             }
         }
 
         for (Node<T> n : testGraph.getNodes()) {
             if (!refGraph.getNodes().contains(n)) {
-                missingOrTooMany.get(2).add(n);
+                graphItem.surplusNodes.add(n);
             }
         }
 
         for (Edge<T> e : testGraph.getEdges()) {
             if (!refGraph.getEdges().contains(e)) {
-                missingOrTooMany.get(3).add(e);
+                graphItem.surplusEdges.add(e);
             }
         }
 
-        return missingOrTooMany;
+        return graphItem;
     }
 
     public static <T> boolean isEquivalent(Graph<T> refGraph, Graph<T> testGraph) {
-        for (List<GraphItem<T>> l : missingAndRedundantNodes(refGraph, testGraph)) {
-            if (!l.isEmpty()) {
-                return false;
-            }
-        }
+        GraphItem<T> graphItem = missingAndRedundantNodes(refGraph, testGraph);
 
-        return true;
+        return (graphItem.missingNodes.isEmpty() &&
+                graphItem.missingEdges.isEmpty() &&
+                graphItem.surplusNodes.isEmpty() &&
+                graphItem.surplusEdges.isEmpty());
     }
 
 }
