c6acdd093a377217baeac1435fd358801bf1d348
betterNameStillPending <user2537@pm.me>
2025-01-20 11:57:46 +0100

    Realised requested changes to GraphEquivalency. GraphItem is now an object containing 4 lists. (#33)

--- src/main/java/DOT/API/GraphItem.java
+++ src/main/java/DOT/API/GraphItem.java
@@ -1,4 +1,13 @@
 package DOT.API;
 
-public interface GraphItem<T> {
+import java.util.LinkedList;
+
+public class GraphItem<T> {
+    LinkedList<Node<T>> missingNodes = new LinkedList<>();
+    LinkedList<Node<T>> surplusNodes = new LinkedList<>();
+
+    LinkedList<Edge<T>> missingEdges = new LinkedList<>();
+    LinkedList<Edge<T>> surplusEdges = new LinkedList<>();
+
+    public GraphItem(){}
 }
