b2b57544e5b886e8b2f7f532450de5b36f0aff9d
betterNameStillPending <user2537@pm.me>
2025-01-23 16:56:46 +0100

    Small fix

--- src/test/java/SootAPITest.java
+++ src/test/java/SootAPITest.java
@@ -1,6 +1,7 @@
 import SootAPI.AnalysedClass;
 import SootAPI.AnalysedPackage;
 import SootAPI.Util;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedList;
@@ -8,16 +9,20 @@ import java.util.List;
 import java.util.Objects;
 
 public class SootAPITest {
+    static AnalysedPackage analysedPackage;
+
+    @BeforeAll
+    public static void before(){
+        analysedPackage = Util.loadPackage("src/test/sources");
+    }
+
     @Test
     public void loadPackage(){
-        AnalysedPackage analysedPackage = Util.loadPackage("src/test/sources");
         assert(!analysedPackage.getClasses().isEmpty());
     }
 
     @Test
     public void allClassesFound(){
-        AnalysedPackage analysedPackage = Util.loadPackage("src/test/sources");
-
         List<String> names = new LinkedList<>();
         for (AnalysedClass c : analysedPackage.getClasses()) {
             names.add(c.getName());
@@ -33,8 +38,7 @@ public class SootAPITest {
     }
 
     @Test
-    public void abstractFound(){
-        AnalysedPackage analysedPackage = Util.loadPackage("src/test/sources");
+    public void abstractClassFound(){
         AnalysedClass[] analysedClasses = analysedPackage.getClasses().toArray(new AnalysedClass[0]);
         int i = 0;
         while(!Objects.equals(analysedClasses[i].getName(), "NodeAbs")) i++;
@@ -44,7 +48,6 @@ public class SootAPITest {
 
     @Test
     public void interfaceFound(){
-        AnalysedPackage analysedPackage = Util.loadPackage("src/test/sources");
         AnalysedClass[] analysedClasses = analysedPackage.getClasses().toArray(new AnalysedClass[0]);
         int i = 0;
         while(!Objects.equals(analysedClasses[i].getName(), "QueueInt")) i++;
