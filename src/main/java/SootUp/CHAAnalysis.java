fcaa69c4d23123163f571c7865dbe9414958f9b2
Charlotte Pabst <charlotte.pabst@stud.tu-darmstadt.de>
2025-01-29 17:21:24 +0100

    split cha and rta into different subclasses

---
+++ src/main/java/SootUp/CHAAnalysis.java
@@ -0,0 +1,14 @@
+package SootUp;
+
+import sootup.core.types.Type;
+import java.util.Set;
+
+public class CHAAnalysis extends Analysis {
+    public CHAAnalysis() {
+        super();
+    }
+
+    protected Set<Type> getConstructedTypes() {
+        return null;
+    }
+}
