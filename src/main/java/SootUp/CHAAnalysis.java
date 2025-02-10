ac7fe5a3cf7c13fbd90d010abb91d19cadb765f9
TankChomper <nicjoe@outlook.de>
2025-02-10 18:06:36 +0100

    update cha analysis

--- src/main/java/SootUp/CHAAnalysis.java
+++ src/main/java/SootUp/CHAAnalysis.java
@@ -8,6 +8,7 @@ import sootup.core.jimple.common.stmt.Stmt;
 import sootup.core.model.SootClass;
 import sootup.core.model.SootClassMember;
 import sootup.core.model.SootMethod;
+import sootup.core.types.ClassType;
 import sootup.core.types.Type;
 
 import java.util.Optional;
@@ -49,25 +50,13 @@ public class CHAAnalysis extends Analysis {
         // Analyze each statement in the target method
         for (Stmt s : InternalUtil.getStatements(methodToAnalyse)) {
             if (s.containsInvokeExpr()) { // s is function call
-                System.out.println("NEXT STMT");
                 AbstractInvokeExpr callSite = s.getInvokeExpr();
 
-                // Create an edge in the call graph for the function call
-                Edge<String> e = new Edge<>(methodInGraph, new Node<>(callSite.getMethodSignature().getDeclClassType().getClassName()+"_" + callSite.getMethodSignature().getName().replace("<init>", "")));
+                System.out.println("NEXT STMT");
 
-                // Ensure the edge is not already present in the graph before adding it
-                if (!callGraph.getEdges().stream().map(Edge::toString).collect(Collectors.toSet()).contains(e.toString())) {
-                    System.out.println("Adding: " + e);
-                    for (Edge<String> ed : callGraph.getEdges()) {
-                        System.out.println(ed);
-                    }
-                    callGraph.addEdge(e);
-                }
+                Edge<String> e = GetMethodStringEdge(callSite, methodInGraph,callGraph);
 
-                // Ensure the target node is added if not already present
-                if (!callGraph.getNodes().stream().map(Node::toString).collect(Collectors.toSet()).contains(e.getTargetNode().toString())) {
-                    callGraph.addNode(e.getTargetNode());
-                }
+                AddEdge(callGraph, e);
 
             }
         }
@@ -75,6 +64,56 @@ public class CHAAnalysis extends Analysis {
         // Return the constructed call graph wrapped in an Optional
         return Optional.of(callGraph);
     }
+
+
+
+    private static Edge<String> GetMethodStringEdge(AbstractInvokeExpr callSite, Node<String> methodInGraph, Graph<String> callGraph) {
+
+        // Create node for the call graph for the function call
+        Node<String> n = new Node<>(callSite.getMethodSignature().getDeclClassType().getClassName()
+                    + "_" + callSite.getMethodSignature().getName().replace("<init>", ""));
+
+        ClassType c = callSite.getMethodSignature().getDeclClassType();
+        Class<?> superClass = c.getClass().getSuperclass(); //does not work as intended
+
+        System.out.println("SuperClass: " + superClass.getName());
+        if (!superClass.equals(Object.class)) {
+            methodInGraph = new Node<>(c.getClassName());
+//            AddSuperClassEdge(c.getClass(), callGraph);
+        }
+
+        return new Edge<>(methodInGraph, n);
+    }
+
+    private static void AddSuperClassEdge(Class<?> c, Graph<String> callGraph) {
+        Class<?> superClass = c.getSuperclass();
+        if (!superClass.getSuperclass().equals(Object.class)){
+            AddSuperClassEdge(superClass.getSuperclass(), callGraph);
+        }
+
+        Node<String> n1 = new Node<>(c.getName());
+        Node<String> n2 = new Node<>(superClass.getName());
+        Edge<String> e = new Edge<>(n1, n2);
+        AddEdge(callGraph, e);
+
+    }
+
+    private static void AddEdge(Graph<String> callGraph, Edge<String> e) {
+        // Ensure the edge is not already present in the graph before adding it
+        if (!callGraph.getEdges().stream().map(Edge::toString).collect(Collectors.toSet()).contains(e.toString())) {
+            System.out.println("\nAdding: " + e);
+            for (Edge<String> ed : callGraph.getEdges()) {
+                System.out.println(ed);
+            }
+            callGraph.addEdge(e);
+        }
+
+        // Ensure the target node is added if not already present
+        if (!callGraph.getNodes().stream().map(Node::toString).collect(Collectors.toSet()).contains(e.getTargetNode().toString())) {
+            callGraph.addNode(e.getTargetNode());
+        }
+    }
+
 
     /**
      * Retrieves the set of constructed types.
