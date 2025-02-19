fb3314c5dc4c533cf103751bd6a0a4dee0652069
totallynotadolphin <totallynotadolphin@riseup.net>
2025-02-19 15:30:01 +0100

    added skeleton for student implementations, created Härtetest for loadPackage

---
+++ simple/Foo.java
@@ -0,0 +1,11 @@
+public class Foo extends Test {
+
+	public Test test;
+	public int c;
+
+	public Foo(Test test) {
+		super(2,5);
+		this.test = test;
+		this.c = 25;
+	}
+}
---
+++ src/main/java/StudentImplementedCHAAnalysis.java
@@ -0,0 +1,11 @@
+import AST.CodeStructure.Package;
+import DotAPI.Graph;
+
+public class StudentImplementedCHAAnalysis {
+
+    public static Graph<String> run(Package p) {
+        // ...
+        return null;
+    }
+
+}
