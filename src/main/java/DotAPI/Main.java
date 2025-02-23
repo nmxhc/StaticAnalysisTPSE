2e14e8dee4012d1f1266518d6b0ce9d07f5d66ac
TankComper <154629812+TankComper@users.noreply.github.com>
2025-02-23 18:31:10 +0100

    added CHA (#63)

--- src/main/java/DotAPI/Main.java
+++ src/main/java/DotAPI/Main.java
@@ -7,8 +7,20 @@ import java.util.Optional;
 public class Main {
 
     public static void main(String[] args) {
+        Optional<Graph<String>> g = CHAAnalysis.CHA("simple");
+        if (g.isEmpty()) {System.out.print("Error finding file" ); return;}
+        String s = DotFileGenerator.generateDotString(g.get());
 
-        DotToHtmlGenerator.embedDotInHtml("basicTests/basicGraph.dot", "basicTests/HtmlTest.html");
+        try {
+            DotFileGenerator.writeDotFile("output.dot", s);
+        } catch (Exception e) {
+            System.out.printf("Error writing file: %s\n", e.getMessage());
+        }
+
+
+
+
+        //DotToHtmlGenerator.embedDotInHtml("basicTests/basicGraph.dot", "basicTests/HtmlTest.html","graph");
 
     }
 
