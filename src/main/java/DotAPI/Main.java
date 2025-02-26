5d6c7b76f78564b5b0fea6b8966b2f7477c82a35
Charlotte Pabst <charlotte.pabst@stud.tu-darmstadt.de>
2025-02-26 01:38:37 +0100

    SootUp: fix CHA analysis

--- src/main/java/DotAPI/Main.java
+++ src/main/java/DotAPI/Main.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 public class Main {
 
     public static void main(String[] args) {
-        Optional<Graph<String>> g = CHAAnalysis.CHA("simple");
+        Optional<Graph<String>> g = new CHAAnalysis("simple").run("Test", "test");
         if (g.isEmpty()) {System.out.print("Error finding file" ); return;}
         String s = DotFileGenerator.generateDotString(g.get());
 
