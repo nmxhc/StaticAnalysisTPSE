13a9974dc4351ca7865d8330c212938d77490e9a
betterNameStillPending <user2537@pm.me>
2025-02-28 15:03:16 +0100

    redundant javax import?

--- src/main/java/ReferenceImplementations/RTAReference.java
+++ src/main/java/ReferenceImplementations/RTAReference.java
@@ -16,7 +16,7 @@ import AST.Types.Type;
 import DotAPI.Graph;
 import DotAPI.Node;
 
-import javax.swing.plaf.nimbus.State;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
